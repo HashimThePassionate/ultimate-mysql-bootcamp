786ecfc370706bf9d702b1d61d87983e7d06d283
Muhammad Hashim <hashiimtahir@gmail.com>
2023-12-24 20:35:50 +0500

    Inner JOiN

---
+++ SQL Queries/orders_items.sql
@@ -0,0 +1,57 @@
+create table orders_items (
+	order_items INT,
+	order_id VARCHAR(50),
+	product_id VARCHAR(50),
+	unit_price DECIMAL(5,3),
+	quantity VARCHAR(50)
+);
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (1, '1', '2', 36.147, '3');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (2, '4', '9', 5.99, '9');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (3, '3', '8', 21.438, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (4, '5', '8', 36.867, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (5, '2', '5', 8.37, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (6, '1', '1', 6.941, '3');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (7, '6', '9', 3.572, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (8, '8', '2', 21.387, '2');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (9, '3', '6', 46.175, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (10, '7', '6', 19.385, '4');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (11, '5', '6', 44.196, '9');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (12, '9', '6', 3.315, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (13, '4', '8', 5.035, '2');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (14, '2', '9', 45.38, '3');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (15, '1', '3', 20.855, '3');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (16, '7', '7', 42.817, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (17, '1', '7', 25.307, '6');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (18, '8', '4', 30.345, '2');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (19, '8', '7', 14.453, '5');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (20, '6', '1', 1.664, '5');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (21, '3', '4', 34.023, '5');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (22, '5', '4', 36.589, '5');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (23, '6', '2', 28.382, '5');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (24, '1', '8', 13.189, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (25, '1', '2', 48.605, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (26, '3', '1', 22.087, '6');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (27, '1', '6', 31.875, '9');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (28, '3', '8', 43.655, '9');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (29, '9', '3', 21.86, '4');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (30, '7', '1', 4.678, '2');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (31, '1', '4', 6.637, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (32, '5', '2', 5.121, '9');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (33, '9', '7', 6.19, '4');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (34, '2', '4', 16.922, '5');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (35, '3', '6', 18.546, '9');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (36, '4', '9', 10.216, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (37, '1', '2', 27.242, '2');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (38, '2', '6', 20.16, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (39, '3', '8', 10.435, '3');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (40, '2', '6', 41.261, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (41, '5', '8', 3.484, '6');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (42, '7', '1', 30.547, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (43, '1', '5', 2.506, '8');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (44, '8', '3', 13.685, '6');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (45, '5', '5', 3.492, '7');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (46, '2', '2', 31.315, '1');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (47, '1', '6', 24.298, '1');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (48, '1', '9', 4.941, '4');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (49, '8', '1', 21.804, '1');
+insert into orders_items (order_items, order_id, product_id, unit_price, quantity) values (50, '9', '4', 13.147, '2');
