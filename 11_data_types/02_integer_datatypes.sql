5f8a7425804c0f485e1743793630ac255e35c1ba
#𝑳𝒂𝒓𝒂𝒊𝒃-._.-𝑰𝒔𝒉𝒕𝒊𝒂𝒒! <159939710+Techwiz-Laraib@users.noreply.github.com>
2024-05-09 22:12:36 +0500

    Update 02_integer_datatypes.sql

--- 11_data_types/02_integer_datatypes.sql
+++ 11_data_types/02_integer_datatypes.sql
@@ -1,6 +1,4 @@
-<<<<<<< HEAD:11_data_types/02_integers_datatypes.sql
 
-=======
 -- Inserting sample data into the table
 INSERT INTO example_integers (age, score, quantity) VALUES
 (25, 85, 100),     -- Inserting values for a TINYINT, INT, and BIGINT respectively
@@ -26,6 +24,5 @@ SELECT SUM(score) AS total_score FROM example_integers;
 SELECT MAX(age) AS max_age FROM example_integers;
 
 
->>>>>>> 213b51e59b698c81059e981578f8f21ba8978e9a:11_data_types/02_integers_datatypes
 
 
