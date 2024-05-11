c2b20e71ca431cbbbe19c1b5fb81f77a95044987
#𝑳𝒂𝒓𝒂𝒊𝒃-._.-𝑰𝒔𝒉𝒕𝒊𝒂𝒒! <159939710+Techwiz-Laraib@users.noreply.github.com>
2024-05-11 19:38:42 +0500

    Update 00_string_datatypes.sql

--- 11_data_types/00_string_datatypes/00_string_datatypes.sql
+++ 11_data_types/00_string_datatypes/00_string_datatypes.sql
@@ -14,3 +14,14 @@ CREATE TABLE `customers` (
   `points` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`customer_id`)
 );
+
+INSERT INTO `customers` VALUES (1,'Babara','MacCaffrey','1986-03-28','781-932-9754','0 Sage Terrace','Waltham','MA',2273);
+INSERT INTO `customers` VALUES (2,'Ines','Brushfield','1986-04-13','804-427-9456','14187 Commercial Trail','Hampton','VA',947);
+INSERT INTO `customers` VALUES (3,'Freddi','Boagey','1985-02-07','719-724-7869','251 Springs Junction','Colorado Springs','CO',2967);
+INSERT INTO `customers` VALUES (4,'Ambur','Roseburgh','1974-04-14','407-231-8017','30 Arapahoe Terrace','Orlando','FL',457);
+INSERT INTO `customers` VALUES (5,'Clemmie','Betchley','1973-11-07',NULL,'5 Spohn Circle','Arlington','TX',3675);
+INSERT INTO `customers` VALUES (6,'Elka','Twiddell','1991-09-04','312-480-8498','7 Manley Drive','Chicago','IL',3073);
+INSERT INTO `customers` VALUES (7,'Ilene','Dowson','1964-08-30','615-641-4759','50 Lillian Crossing','Nashville','TN',1672);
+INSERT INTO `customers` VALUES (8,'Thacher','Naseby','1993-07-17','941-527-3977','538 Mosinee Center','Sarasota','FL',205);
+INSERT INTO `customers` VALUES (9,'Romola','Rumgay','1992-05-23','559-181-3744','3520 Ohio Trail','Visalia','CA',1486);
+INSERT INTO `customers` VALUES (10,'Levy','Mynett','1969-10-13','404-246-3370','68 Lawn Avenue','Atlanta','GA',796);
