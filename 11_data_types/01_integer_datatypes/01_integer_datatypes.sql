86b5bc626fa75abbf81b7454eaafb6d087c026df
#𝑳𝒂𝒓𝒂𝒊𝒃-._.-𝑰𝒔𝒉𝒕𝒊𝒂𝒒! <159939710+Techwiz-Laraib@users.noreply.github.com>
2024-05-11 19:50:06 +0500

    Update 01_integer_datatypes.sql

--- 11_data_types/01_integer_datatypes/01_integer_datatypes.sql
+++ 11_data_types/01_integer_datatypes/01_integer_datatypes.sql
@@ -1,20 +1,9 @@
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `order_statuses` (
+  `order_status_id` tinyint(4) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `customer_id` int(11) NOT NULL,
-  `order_date` date NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '1',
-  `comments` varchar(2000) DEFAULT NULL,
-  `shipped_date` date DEFAULT NULL,
-  `shipper_id` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`order_id`),
-
-INSERT INTO `orders` VALUES (1,6,'2019-01-30',1,NULL,NULL,NULL);
-INSERT INTO `orders` VALUES (2,7,'2018-08-02',2,NULL,'2018-08-03',4);
-INSERT INTO `orders` VALUES (3,8,'2017-12-01',1,NULL,NULL,NULL);
-INSERT INTO `orders` VALUES (4,2,'2017-01-22',1,NULL,NULL,NULL);
-INSERT INTO `orders` VALUES (5,5,'2017-08-25',2,'','2017-08-26',3);
-INSERT INTO `orders` VALUES (6,10,'2018-11-18',1,'Aliquam erat volutpat. In congue.',NULL,NULL);
-INSERT INTO `orders` VALUES (7,2,'2018-09-22',2,NULL,'2018-09-23',4);
-INSERT INTO `orders` VALUES (8,5,'2018-06-08',1,'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',NULL,NULL);
-INSERT INTO `orders` VALUES (9,10,'2017-07-05',2,'Nulla mollis molestie lorem. Quisque ut erat.','2017-07-06',1);
-INSERT INTO `orders` VALUES (10,6,'2018-04-22',2,NULL,'2018-04-23',2);
+  PRIMARY KEY (`order_status_id`)
+);
+INSERT INTO `order_statuses` VALUES (1,'Processed');
+INSERT INTO `order_statuses` VALUES (2,'Shipped');
+INSERT INTO `order_statuses` VALUES (3,'Delivered');
