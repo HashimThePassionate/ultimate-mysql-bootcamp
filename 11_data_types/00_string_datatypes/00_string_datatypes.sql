fde73c01873982eb8d8514602b15d64c2f2af9f6
#𝑳𝒂𝒓𝒂𝒊𝒃-._.-𝑰𝒔𝒉𝒕𝒊𝒂𝒒! <159939710+Techwiz-Laraib@users.noreply.github.com>
2024-05-11 19:18:37 +0500

    Update  00_string_datatypes.sql

---
+++ 11_data_types/00_string_datatypes/00_string_datatypes.sql
@@ -0,0 +1,16 @@
+
+CREATE DATABASE `sql_hr`;
+USE `sql_hr`;
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `birth_date` date DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `address` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` char(2) NOT NULL,
+  `points` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`customer_id`)
+);
